3b020dc62e7d9dd1460bd9be9cc29f24ecf27849
parknuri777 <parknuri777@gmail.com>
2024-04-29 16:19:55 +0900

    한줄 추가

--- src/helloworld_20240429/Hello.java
+++ src/helloworld_20240429/Hello.java
@@ -5,7 +5,8 @@ public class Hello {
 	public static void main(String[] args) {  //메인 함수 -> 이게 있어야 바로 실행가능 
 		// TODO Auto-generated method stub
 		System.out.println("HelloWorld!!");  //콘솔에 결과를 프린트-> 주의 :"" 큰따옴표만 문자열에 사용
-		System.out.println("안녕하세요!!"); 
+		System.out.println("안녕하세요!!");
+		System.out.println("안녕하세요!!");
 	}
 
 }
